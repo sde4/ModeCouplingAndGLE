7b80863fd3e8ff989406ab4608918f84455b1c71
sde4 <sde4@illinois.edu>
2018-07-07 18:05:51 -0500

    Changes after defining structures

--- ForceSys.c
+++ ForceSys.c
@@ -11,7 +11,7 @@ double	ForceSys(sys_var s, run_param r, int j)
 	double 	q_j, q_k;
 	double 	f_t, f2_t, alpha;
 
-	fr 			= gsl_vector_get(s.frvec, j);
+	fr 		= gsl_vector_get(s.frvec, j);
 	q_j 		= gsl_vector_get(s.qvec, j);
 
 	f_t 		= -pow(2*PI*fr, 2.0)*q_j;
